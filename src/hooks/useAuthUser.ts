9ace39cff2b404ac2ffe2b464bc849c4ebfad72c
이정환 <king199777@gmail.com>
2022-12-26 10:52:53 +0900

    [Feat] add useAuthUser Hook
    -

---
+++ src/hooks/useAuthUser.ts
@@ -0,0 +1,8 @@
+import { RootState } from "@store/createStore";
+import { common_types } from "@types";
+import { useSelector } from "react-redux";
+
+export default function useAuthUser(): common_types.AuthUser {
+  const auth = useSelector((v: RootState) => v.auth) as common_types.AuthUser;
+  return auth;
+}
