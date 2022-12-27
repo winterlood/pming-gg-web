356f608f4960b05e360c384a549c70d26d9a9472
이정환 <king199777@gmail.com>
2022-12-27 12:02:33 +0900

    [Feat] Indi&Business HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,33 +3,182 @@ import MagazineCraousel from "@components/MagazineCarousel";
 import withOnlyProfiledUser from "@hoc/withOnlyProfiledUser";
 import useAuthUser from "@hooks/useAuthUser";
 import Layout from "@layout/Layout";
-import { RootState } from "@store/createStore";
-import { api_types, common_types } from "@types";
-import { useSelector } from "react-redux";
+import { ReactElement } from "react";
 
-function IndividualHome() {
-  return <div></div>;
+import classNames from "classnames/bind";
+import style from "./HomePage.module.scss";
+import { common_types } from "@types";
+import useGetJobPostByUserIdQuery from "@hooks/useGetJobPostByUserIdQuery";
+import JobPostItem from "@components/JobPostItem";
+import useGetRecommendDevlopers from "@hooks/useGetRecommendDevlopers";
+import DeveloperItem from "@components/DeveloperItem";
+import { useNavigate } from "react-router-dom";
+import useGetProfileQuery from "@hooks/useGetProfileQuery";
+import GitHubLanaguageChart from "@components/GitHubLanaguageChart";
+import useGetRecommendedJobPost from "@hooks/useGetRecommendedJobPost";
+const cx = classNames.bind(style);
+
+function HomeSection(props: {
+  title: string;
+  viewMore?: {
+    text: string;
+    onClick: React.MouseEventHandler<HTMLSpanElement>;
+  };
+  children: ReactElement | ReactElement[] | undefined;
+  bodyClassName?: string;
+}) {
+  return (
+    <div className={cx("HomeSection")}>
+      <div className={cx("header")}>
+        <div className={cx("title")}>{props.title}</div>
+        {props.viewMore && (
+          <div className={cx("viewmore")}>
+            <span onClick={props.viewMore.onClick}>{props.viewMore.text}</span>
+          </div>
+        )}
+      </div>
+      <div className={[cx("body"), props.bodyClassName].join(" ")}>
+        {props.children}
+      </div>
+    </div>
+  );
 }
 
-function BusinessHome() {
-  return <div></div>;
+function IndividualHome({ user }: { user: common_types.AuthUser["user"] }) {
+  const nav = useNavigate();
+  const id = user.id;
+
+  const { data: profileQueryData, isLoading: profileQueryLoading } =
+    useGetProfileQuery(id as string, {
+      refetchOnMount: true,
+      enabled: !!id,
+    });
+
+  const { data: jobPostList, isLoading } = useGetRecommendedJobPost("", {
+    refetchOnMount: true,
+  });
+
+  console.log(profileQueryData);
+
+  return (
+    <>
+      <HomeSection
+        title="내 코드"
+        bodyClassName={cx("list_section")}
+        viewMore={{
+          text: "더보기",
+          onClick: () => nav(`/profile/${id}`),
+        }}
+      >
+        {profileQueryData && (
+          <div>
+            <GitHubLanaguageChart
+              type="pie"
+              languageData={profileQueryData.github_user.totalLangList}
+            />
+          </div>
+        )}
+      </HomeSection>
+      <HomeSection
+        title="추천 채용 공고"
+        bodyClassName={cx("list_section")}
+        viewMore={{
+          text: "더보기",
+          onClick: () => nav(`/jobs`),
+        }}
+      >
+        {jobPostList &&
+          jobPostList
+            .slice(0, 2)
+            .map((jobPost) => <JobPostItem key={jobPost.id} {...jobPost} />)}
+      </HomeSection>
+    </>
+  );
+}
+
+function BusinessHome({ user }: { user: common_types.AuthUser["user"] }) {
+  const nav = useNavigate();
+  const id = user.id;
+
+  const { data: jobPostQueryData, isLoading: jobPostQueryLoading } =
+    useGetJobPostByUserIdQuery(id as string, {
+      refetchOnMount: true,
+      enabled: !!id,
+    });
+
+  const { data: developerList, isLoading } = useGetRecommendDevlopers({
+    refetchOnMount: true,
+  });
+
+  return (
+    <>
+      <HomeSection
+        title="내 공고"
+        bodyClassName={cx("list_section")}
+        viewMore={{
+          text: "더보기",
+          onClick: () => nav(`/profile/${id}`),
+        }}
+      >
+        {jobPostQueryData &&
+          jobPostQueryData
+            ?.slice(0, 2)
+            .map((jobPost) => <JobPostItem key={jobPost.id} {...jobPost} />)}
+      </HomeSection>
+      <HomeSection
+        title="인재 추천"
+        bodyClassName={cx("list_section")}
+        viewMore={{
+          text: "더보기",
+          onClick: () => nav(`/developers`),
+        }}
+      >
+        {developerList &&
+          developerList
+            ?.slice(0, 2)
+            .map((developer) => (
+              <DeveloperItem
+                key={developer.id}
+                {...developer}
+                name={developer.username}
+                bio={developer.user_detail_individual?.bio as string}
+              />
+            ))}
+      </HomeSection>
+    </>
+  );
 }
 
 function HomePage() {
+  const nav = useNavigate();
   const { user } = useAuthUser();
-
+  if (!user) return null;
   return (
     <Layout
       header={{
         pageName: "홈",
         rightButton: (
-          <IconButton type={"light"} icon={"alarm"} onClick={() => {}} />
+          <IconButton
+            type={"light"}
+            icon={"alarm"}
+            onClick={() => nav(`/alarm`)}
+          />
         ),
       }}
       footer
     >
-      <div>
-        <MagazineCraousel />
+      <div className={cx("container")}>
+        <HomeSection
+          title="매거진"
+          viewMore={{
+            text: "더보기",
+            onClick: () => nav(`/magazine`),
+          }}
+        >
+          <MagazineCraousel />
+        </HomeSection>
+        {user.user_type === "individual" && <IndividualHome user={user} />}
+        {user.user_type === "business" && <BusinessHome user={user} />}
       </div>
     </Layout>
   );
